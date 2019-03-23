b438c2e23d11b7f4e7554968989796b348d8efeb
James Douglas <james@douglastechnology.co.uk>
2019-03-23 18:05:28 +0000

    commit

--- src/edit.js
+++ src/edit.js
@@ -15,8 +15,7 @@ $(function() {
   browser.storage.local.get().then(function(item) { 
     if (item.styling_1) {
       $('#style_name').val(item.styling_1.name);
-      if (!item.styling_1.options) {  }
-      var blocks = objectLength(item.styling_1) - 1;
+      var blocks = objectLength(item.styling_1) - 3;
       for (var e = 1; e <= blocks; e++) {
         if (blocks > 1 && e > 1) { $('#content > .block:last-of-type > .add_block').click(); }
         var urls = (objectLength(item.styling_1["block_"+e]) - 1) / 2;
@@ -40,8 +39,8 @@ $(function() {
   updateBlocks();
   $('#save').click(function() {
     if ($('#style_name').val()) {
-      browser.storage.local.get().then(function(item) { 
-        $.extend(true, item, { styling_1: { name: $('#style_name').val() } }); 
+      browser.storage.local.get().then(function(item) {
+        $.extend(true, item, { styling_1: { name: $('#style_name').val(), options: { tab_size: 2, font_size: 12, smart_indent: true, tab_indent: false, auto_close: true, theme: "theme-crimson_editor.js", keybinding: "default" } } }); 
         for (var c = 1; c <= $('div.block').length; c++) {
           var blockName = "block_"+c, urls = $('div.block:nth-of-type('+c+')').children('section').length;
           $.extend(true, item, { styling_1: { [blockName]: { code: ace.edit("code_"+c).getValue().replace(/^|\s+$/g, '') } } });

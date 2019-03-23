a68344b98ab43e9a3677eca3c4cd15ed8881955b
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:41:03 +0000

    commit

--- src/edit.js
+++ src/edit.js
@@ -45,10 +45,7 @@ $(function() {
   updateBlocks();
   $('#save').click(function() {
     if ($('#style_name').val()) {
-      var extendedItem;
-      browser.storage.local.get().then(function(item) { $.extend(true, item, { styling_1: { name: $('#style_name').val() } }); extendedItem = item; });
-      alert(JSON.stringify(extendedItem));
-      browser.storage.local.set(extendedItem);
+      browser.storage.local.get().then(function(item) { $.extend(true, item, { styling_1: { name: $('#style_name').val() } }); browser.storage.local.set(item).then(onChange, onError); });
       alert('pre-loop');
       for (var c = 1; c <= $('div.block').length; c++) {
         var blockName = "block_"+c, saved_code = { styling_1: { [blockName]: { code: ace.edit("code_"+c).getValue().replace(/^|\s+$/g, '') } } }, urls = $('div.block:nth-of-type('+c+')').children('section').length;

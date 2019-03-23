035a651d2a1684a8c998ee38c02b9139c8bd6d0c
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:45:57 +0000

    commit

--- src/manage.js
+++
@@ -1,9 +0,0 @@
-function onChange(item) {}
-function onError(error) { console.log(`Error: ${error}`); }
-function sendMessageToTabs(tabs) {
-  for (let tab of tabs) {
-    browser.tabs.sendMessage(tab.id, { message: "update scripts" }).then(response => {}).catch(onError);
-  }
-}
-$(function() {
-});

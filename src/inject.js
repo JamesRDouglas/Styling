068dd99744c4cbf5ddad21cd055d3bb75c55a02d
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:11:59 +0000

    commit

--- src/inject.js
+++ src/inject.js
@@ -2,16 +2,23 @@
 function updateStyles() {
   let localStorage = browser.storage.local.get(function(item) {
     if (item.styling_1) {
-      if (item.disabled == "false") {
-        var styleElement = document.createElement("style");
-        styleElement.setAttribute("id", "styling-1");
-        styleElement.setAttribute("class", "styling");
-        styleElement.setAttribute("type", "text/css");
-        styleElement.appendChild(document.createTextNode(item.styling_1.code));
-        document.getElementsByTagName('html')[0].appendChild(styleElement);
-      } else {
-        var elements = document.getElementsByClassName('styling');
-        for (var i = 0; i < elements.length; i++) { document.getElementsByTagName("html")[0].removeChild(elements[i]); }
+      function apply() {
+        if (item.disabled == "false") {
+          var styleElement = document.createElement("style");
+          styleElement.setAttribute("id", "styling-1");
+          styleElement.setAttribute("class", "styling");
+          styleElement.setAttribute("type", "text/css");
+          styleElement.appendChild(document.createTextNode(item.styling_1.code));
+          document.getElementsByTagName('html')[0].appendChild(styleElement);
+        } else {
+          var elements = document.getElementsByClassName('styling');
+          for (var i = 0; i < elements.length; i++) { document.getElementsByTagName("html")[0].removeChild(elements[i]); }
+        }
+      }
+      if (item.styling_1.url && item.styling_1.url == window.location.href) {
+        apply();
+      } else if (item.styling_1.url && item.styling_1.url == "") {
+        apply();
       }
     }
   });

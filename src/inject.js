0446e39913195f3aafb62bf2115294dea4f1101d
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:18:21 +0000

    commit

--- src/inject.js
+++ src/inject.js
@@ -1,6 +1,8 @@
 "use strict";
 function onChange() { }
 function onError(error) { console.log(`Error: ${error}`); }
+function objectLength(object) { var length = 0; for(var key in object) { if( object.hasOwnProperty(key) ) { ++length; } } return length; };
+function insertAfter(el, referenceNode) { referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling); }
 function getDomain(url) {
   url = url.replace(/(https?:\/\/)?(www.)?/i, '');
   if (url.indexOf('/') !== -1) { return url.split('/')[0]; }
@@ -14,14 +16,17 @@ function updateStyles() {
       var elements = document.getElementsByClassName('styling');
       for (var i = 0; i < elements.length; i++) { document.getElementsByTagName("html")[0].removeChild(elements[i]); }
       if (item.disabled == "false") {
-      	var regularExpression = new RegExp(item.styling_1.block_1.url_1);
-        if (item.styling_1.block_1.url_1 != undefined && ((item.styling_1.block_1.url_1_type == "url" && item.styling_1.block_1.url_1 == window.location.href) || (item.styling_1.block_1.url_1_type == "starting" && window.location.href.startsWith(item.styling_1.block_1.url_1)) || (item.styling_1.block_1.url_1_type == "domain" && item.styling_1.block_1.url_1 == getDomain(window.location.href)) || (item.styling_1.block_1.url_1_type == "regexp" &&  regularExpression.test(window.location.href)) || (item.styling_1.block_1.url_1_type == "everything"))) {
-          var styleElement = document.createElement("style");
-          styleElement.setAttribute("id", "styling-1");
-          styleElement.setAttribute("class", "styling");
-          styleElement.setAttribute("type", "text/css");
-          styleElement.appendChild(document.createTextNode(item.styling_1.block_1.code.replace(/(\r\n\t|\n|\r\t)/gm,"")));
-          document.getElementsByTagName('html')[0].appendChild(styleElement);
+        var urls = (objectLength(item.styling_1.block_1) - 1) / 2;
+        for (a = 1; a <= urls; a++) { 
+          if (item.styling_1.block_1["url_" + a] != undefined && ((item.styling_1.block_1["url_" + a + "_type"] == "url" && item.styling_1.block_1["url_" + a] == window.location.href) || (item.styling_1.block_1["url_" + a + "_type"] == "starting" && window.location.href.startsWith(item.styling_1.block_1["url_" + a])) || (item.styling_1.block_1["url_" + a + "_type"] == "domain" && item.styling_1.block_1["url_" + a] == getDomain(window.location.href)) || (item.styling_1.block_1["url_" + a + "_type"] == "everything"))) {
+            var styleElement = document.createElement("style");
+            styleElement.setAttribute("id", "styling-" + a);
+            styleElement.setAttribute("class", "styling");
+            styleElement.setAttribute("type", "text/css");
+            styleElement.appendChild(document.createTextNode(item.styling_1.block_1.code.replace(/(\r\n\t|\n|\r\t)/gm,"")));
+            insertAfter(styleElement, document.getElementsByTagName('html')[0]);
+            //document.getElementsByTagName('html')[0].appendChild(styleElement);
+          }
         }
       }
     }

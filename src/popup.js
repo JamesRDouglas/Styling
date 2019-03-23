a345348922e8e457d1a0f1890259643b8cda3bd2
James Douglas <james@douglastechnology.co.uk>
2019-03-23 18:43:40 +0000

    commit

--- src/popup.js
+++ src/popup.js
@@ -4,26 +4,33 @@ browser.storage.local.get(function(item) {
 });
 function onChange() { }
 function onError(error) { console.log(`${error}`); }
-function getSubdomain(url) { url = url.replace(/(https?:\/\/)?(www.)?/i, ''); if (url.indexOf('/') !== -1) { return url.split('/')[0]; } return url; }
+function getURL() { browser.tabs.query({currentWindow: true, active: true}).then(function(tabs) { for (let tab of tabs) { return tab.url; }; }); }
 function sendDisableToTabs(tabs) { for (let tab of tabs) { browser.tabs.sendMessage(tab.id, {message: "all styles disabled"}).then(response => {  }).catch(onError); } }
 function sendEnableToTabs(tabs) { for (let tab of tabs) { browser.tabs.sendMessage(tab.id, {message: "all styles enabled"}).then(response => {  }).catch(onError); } }
+function getDomain(url, subdomain) {
+  subdomain = subdomain || false;
+  url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+  if (!subdomain) { url = url.split('.').slice(url.length - 2).join('.'); }
+  if (url.indexOf('/') !== -1) { return url.split('/')[0]; }
+  return url;
+}
 $.getJSON('../manifest.json', function(data) { $('#version').text(data.version); });
 $('#disable').change(function() {
   if ($(this).is(':checked')) {
     $('img').prop("src", "../images/StylingDisabled.png");
     browser.browserAction.setIcon({path: "../images/StylingDisabled.png"});
     browser.storage.local.set({ disabled: "true" }).then(onChange, onError);
-    browser.tabs.query({ currentWindow: true }).then(sendDisableToTabs).catch(onError);
+    browser.tabs.query({currentWindow: true}).then(sendDisableToTabs).catch(onError);
   } else {
     $('img').prop("src", "../images/Styling.png");
     browser.browserAction.setIcon({path: "../images/Styling.png"});
     browser.storage.local.set({ disabled: "false" }).then(onChange, onError);
-    browser.tabs.query({ currentWindow: true }).then(sendEnableToTabs).catch(onError);
+    browser.tabs.query({currentWindow: true}).then(sendEnableToTabs).catch(onError);
   }
 });
-$('#url').prop('href', browser.extension.getURL("src/manage.html?action=create&type=url&target="+window.location.href));
-$('#domain').prop('href', browser.extension.getURL("src/manage.html?action=create&type=domain&target="+window.hostname));
-$('#subdomain').prop('href', browser.extension.getURL("src/manage.html?action=create&type=domain&target="+getSubdomain(window.location.href)));
+$('#url').prop('href', browser.extension.getURL("src/manage.html?action=create&type=url&target='"+getURL()+"'"));
+$('#domain').prop('href', browser.extension.getURL("src/manage.html?action=create&type=domain&target='"+getDomain(getURL(), false)+"'"));
+$('#subdomain').prop('href', browser.extension.getURL("src/manage.html?action=create&type=domain&target='"+getDomain(getURL(), true)+"'"));
 $(function() {
   $('img, label').click(function() { $('#disable').click(); });
   $('#manage').click(function() { browser.tabs.create({ url: "manage.html" }).then(onChange, onError); window.close(); });

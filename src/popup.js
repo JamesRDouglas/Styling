aa8a3b70ecfffb6397ed31428c92b26402e200e3
James Douglas <james@douglastechnology.co.uk>
2019-03-23 18:46:56 +0000

    commit

--- src/popup.js
+++ src/popup.js
@@ -28,34 +28,43 @@ $('#disable').change(function() {
     browser.tabs.query({currentWindow: true}).then(sendEnableToTabs).catch(onError);
   }
 });
-//$('#url').prop('href', browser.extension.getURL("src/manage.html?create=url&target="));
-//$('#domain').prop('href', browser.extension.getURL("src/manage.html?create=domain&target="));
-//$('#subdomain').prop('href', browser.extension.getURL("src/manage.html?create=domain&target="));
-  var active_styles = [];
-  browser.tabs.query({currentWindow: true, active: true}).then(function(tabs) { currentURL = tabs[0].url; });
-  browser.storage.local.get(function(item) {
-    var styles = objectLength(item) - 1;
-    for (var b = 1; b <= styles; b++) {
-      var blocks = objectLength(item["styling_"+b]) - 2;
-      block:
-      for (var c = 1; c <= blocks; c++) {
-        var urls = (objectLength(item["styling_"+b]["block_"+c]) - 1) / 2;
-        for (var d = 1; d <= urls; d++) { 
-          if (item["styling_"+b].block_1["url_"+d] != undefined && ((item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "url" && item["styling_"+b]["block_"+c]["url_"+d] == currentURL) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "starting" && currentURL.startsWith(item["styling_"+b]["block_"+c]["url_"+d])) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "domain" && item["styling_"+b]["block_"+c]["url_"+d] == getDomain(currentURL)) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "everything"))) {
-            var styleTitle = "style_"+b+"_name";
-            $.extend(true, active_styles, { [styleTitle]: item["styling_"+b].name });
-            break block;
-          }
+$('#url').prop('href', browser.extension.getURL("src/manage.html?create=url&target="));
+$('#domain').prop('href', browser.extension.getURL("src/manage.html?create=domain&target="));
+$('#subdomain').prop('href', browser.extension.getURL("src/manage.html?create=domain&target="));
+var active_styles = [];
+browser.tabs.query({currentWindow: true, active: true}).then(function(tabs) { currentURL = tabs[0].url; });
+browser.storage.local.get(function(item) {
+  var styles = objectLength(item) - 1;
+  for (var b = 1; b <= styles; b++) {
+    var blocks = objectLength(item["styling_"+b]) - 2;
+    block:
+    for (var c = 1; c <= blocks; c++) {
+      var urls = (objectLength(item["styling_"+b]["block_"+c]) - 1) / 2;
+      for (var d = 1; d <= urls; d++) { 
+        if (item["styling_"+b].block_1["url_"+d] != undefined && ((item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "url" && item["styling_"+b]["block_"+c]["url_"+d] == currentURL) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "starting" && currentURL.startsWith(item["styling_"+b]["block_"+c]["url_"+d])) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "domain" && item["styling_"+b]["block_"+c]["url_"+d] == getDomain(currentURL)) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "everything"))) {
+          var styleTitle = "style_"+b+"_name";
+          $.extend(true, active_styles, { [styleTitle]: item["styling_"+b].name });
+          break block;
         }
       }
     }
-    console.log(active_styles);
-    for (x = 1; x <= objectLength(active_styles); x++) { $('#active-styles').append('<div><input type="checkbox"> <span>'+active_styles["style_"+x+"_name"]+'</span><a href="edit.html?style='+x+'" class="edit">edit</a><a href="#" class="delete" title="Not implemented">delete</a></div>'); }
-  });
+  }
+  console.log(active_styles);
+  for (x = 1; x <= objectLength(active_styles); x++) { $('#active-styles').append('<div><input type="checkbox"> <span>'+active_styles["style_"+x+"_name"]+'</span><a href="edit.html?style='+x+'" class="edit">edit</a><a href="#" class="delete" title="Not implemented">delete</a></div>'); }
+  if (objectLength(active_styles) === 0) { $('#active-styles').append('<i>No styles for this page</i>'); }
+});
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    window.location.reload();
+  }
+}
 $(function() {
   $('img, label').click(function() { $('#disable').click(); });
   $('#manage').click(function() { browser.tabs.create({ url: "manage.html" }).then(onChange, onError); window.close(); });
   $('#details').click(function() { chrome.runtime.openOptionsPage(); window.close(); });
+  $(document).on('click', '#url', function() { browser.tabs.create({ url: $(this).prop('href') }); return false; });
+  $(document).on('click', '#domain', function() { browser.tabs.create({ url: $(this).prop('href') }); return false; });
+  $(document).on('click', '#subdomain', function() { browser.tabs.create({ url: $(this).prop('href') }); return false; });
   $(document).on('click', '.edit', function() { browser.tabs.create({ url: $(this).prop('href') }); return false; });
 });
 

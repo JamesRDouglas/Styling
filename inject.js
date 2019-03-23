145b6fcb274c52f9db28d0ba34b0992de7719a07
James Douglas <james@holyport.local>
2019-03-23 15:08:26 +0000

    commit

--- inject.js
+++ inject.js
@@ -1,6 +1,11 @@
+styleElement = document.createElement("style");
+styleElement.setAttribute("id", "styling-id");
+styleElement.setAttribute("class", "styling");
+styleElement.setAttribute("type", "text/css");
+styleElement.appendChild(document.createTextNode("body { background-color: yellow; }"));
+document.head.appendChild(styleElement);
+
 window.onload = function() {
-	var styleElement = document.getElementById("styling-id");
-	if (styleElement === 0) { return false; }
 	styleElement = document.createElement("style");
 	styleElement.setAttribute("id", "styling-id");
 	styleElement.setAttribute("class", "styling");

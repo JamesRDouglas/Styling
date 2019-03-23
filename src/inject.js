9e61b9101faecc069bc09c3c6ab39aa557806e43
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:11:20 +0000

    commit

--- src/inject.js
+++ src/inject.js
@@ -1,7 +1,21 @@
-styleElement = document.createElement("style");
-styleElement.setAttribute("id", "styling-id");
-styleElement.setAttribute("class", "styling");
-styleElement.setAttribute("type", "text/css");
-styleElement.appendChild(document.createTextNode("body { background-color: yellow; }"));
-document.getElementsByTagName('html')[0].insertAfter(styleElement);
-ROOT.document.getElementsByTagName('html')[0].insertAfter(styleElement);
+"use strict";
+function updateStyles() {
+  let localStorage = browser.storage.local.get(function(item) {
+    if (item.disabled.value == "false") {
+      var styleElement = document.createElement("style");
+      styleElement.setAttribute("id", "styling-id");
+      styleElement.setAttribute("class", "styling");
+      styleElement.setAttribute("type", "text/css");
+      styleElement.appendChild(document.createTextNode("body { background-color: yellow; }"));
+      document.getElementsByTagName('html')[0].appendChild(styleElement);
+    } else {
+      var elements = document.getElementsByClassName('styling');
+      for (var i = 0; i < elements.length; i++) { elements[i].remove(); }
+    }
+  });
+}
+updateStyles();
+browser.runtime.onMessage.addListener(request => {
+  updateStyles();
+  return Promise.resolve({ response: "Hi" });
+});

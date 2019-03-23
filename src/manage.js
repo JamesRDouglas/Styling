9d028529cb31957c75fe39bdaa002936c9d89401
James Douglas <james@douglastechnology.co.uk>
2019-03-23 18:53:20 +0000

    commit

--- src/manage.js
+++ src/manage.js
@@ -6,10 +6,12 @@ function checkStyleExists(b, item, styles_arr) {
   if (item["styling_"+b]) {
     styles_arr.push(b);
     $('#content').append('<div class="style" id="style_'+b+'" data-id="'+b+'"><input type="checkbox"><span class="name" title="'+item["styling_"+b].name+'">'+item["styling_"+b].name+'</span><button class="edit" data-id="'+b+'">Edit</button><button class="delete" data-id="'+b+'">Delete</button><div class="url_list"></div></div>');
+    b++;
   } else {
+    b++;
     checkStyleExists(b, item);
   }
-  b++;
+  return b;
 }
 $(function() {
   var delete_id = new URLSearchParams(window.location.search).get('delete');
@@ -25,7 +27,7 @@ $(function() {
     var styles = objectLength(item) - 1, b = 1;
     for (a = 1; a <= styles; a = a) {
       if (item["styling_"+b] !== undefined) { a++; }
-      checkStyleExists(b, item, styles_arr);
+      b = checkStyleExists(b, item, styles_arr);
     }
   });
   $('#write-new').click(function() { for (a = 1; styles_arr.indexOf(a) === -1; a++) { window.location.href = "edit.html?create="+a; } });

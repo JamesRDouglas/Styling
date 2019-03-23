a107e74facbbd9d78b97af5553a7c86df9134345
James Douglas <james@douglastechnology.co.uk>
2019-03-23 14:54:49 +0000

    Add files via upload

--- inject.js
+++ inject.js
@@ -1,18 +1,6 @@
-var scriptElement = top.window.content.document.createElement('script');
-scriptElement.type = 'text/javascript';
-scriptElement.setAttribute('src','/script.js');
-scriptElement.setAttribute('onload', 'loadStyles()');
-top.window.content.document.getElementsByTagName('head')[0].appendChild(scriptElement);
-
-function loadStyles() {
-	var styleElement = document.getElementById("styling-id");
-	styleElement = document.createElement("style");
-	styleElement.setAttribute("id", "styling-id");
-	styleElement.setAttribute("class", "styling");
-	styleElement.setAttribute("type", "text/css");
-	styleElement.appendChild(document.createTextNode(sections.map(function (section) {
-	    return "body { background-color: yellow; }";
-	}).join("\n")));
-	g_styleElements[styleElement.id] = styleElement;
-	document.documentElement.appendChild(document.importNode(styleElement, true)).disabled = g_disableAll;
-}
+var styleElement = document.createElement("style");
+styleElement.setAttribute("id", "styling-id");
+styleElement.setAttribute("class", "styling");
+styleElement.setAttribute("type", "text/css");
+styleElement.appendChild(document.createTextNode("body { background-color: yellow; }"));
+document.appendChild(document.importNode(styleElement, true));

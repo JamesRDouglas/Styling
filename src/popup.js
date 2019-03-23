25a08e7c7e0736a3bf744390e3288e5323939d1e
James Douglas <james@douglastechnology.co.uk>
2019-03-23 16:19:04 +0000

    commit

--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,20 @@
 $(function() {
-  if (`${item.disabled}` == true) {
+  if (`${item.disabled.state}` == true) {
     $('img').prop("src", "../images/StylingDisabled.png");
   } else {
     $('img').prop("src", "../images/Styling.png");
   }
   $.getJSON('../manifest.json', function(data) { $('#version').text(data.version); });
-  var disabled = false;
+  var disabled = { state: false };
   $('#disable').click(function() {
     if ($(this).is(':checked')) {
       $('img').prop("src", "../images/StylingDisabled.png");
-      disabled = true;
+      disabled = { state: true };
       browser.browserAction.setIcon({path: "../images/StylingDisabled.png"});
       browser.storage.local.set({disabled});
     } else {
       $('img').prop("src", "../images/Styling.png");
-      disabled = false;
+      disabled = { state: false };
       browser.browserAction.setIcon({path: "../images/Styling.png"});
       browser.storage.local.set({disabled});
     }

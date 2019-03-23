e7c9be557cd1113dee3471d5b6521e0bb1cfc737
James Douglas <james@douglastechnology.co.uk>
2019-03-23 18:31:25 +0000

    commit

--- src/edit.js
+++ src/edit.js
@@ -9,11 +9,12 @@ function aceinit() {
   browser.storage.local.get().then(function(item) { 
     if (item.styling_1) { 
       e.setTheme("ace/theme/"+item.styling_1.options.theme);
-      e.setOptions({ maxLines: Infinity, tabSize: Number(item.styling_1.options.tab_size), fontSize: Number(item.styling_1.options.font_size), useSoftTabs: false, fixedWidthGutter: true, printMargin: false, minLines: 15, maxLines: 15, setBehavioursEnabled: item.styling_1.options.smart_indent, enableBasicAutocompletion: item.styling_1.options.autocomplete });
+      e.setOptions({ maxLines: Infinity, tabSize: Number(item.styling_1.options.tab_size), fontSize: Number(item.styling_1.options.font_size), useSoftTabs: false, fixedWidthGutter: true, printMargin: false, minLines: 15, maxLines: 15, enableBasicAutocompletion: item.styling_1.options.autocomplete, enableLiveAutocompletion: item.styling_1.options.autocomplete });
+      e.setBehavioursEnabled(item.styling_1.options.smart_indent);
       if (item.styling_1.options.keybinding !== "default") { e.setKeyboardHandler("ace/keyboard/"+item.styling_1.options.keybinding); }
     } else { 
       e.setTheme("ace/theme/crimson_editor");
-      e.setOptions({ maxLines: Infinity, tabSize: 2, fontSize: 12, useSoftTabs: false, fixedWidthGutter: true, printMargin: false, minLines: 15, maxLines: 15, enableLiveAutocompletion: true });
+      e.setOptions({ maxLines: Infinity, tabSize: 2, fontSize: 12, useSoftTabs: false, fixedWidthGutter: true, printMargin: false, minLines: 15, maxLines: 15, enableBasicAutocompletion: true, enableLiveAutocompletion: true });
     }
   });
   e.getSession().setMode("ace/mode/css");

c6212bbf4dc4cc3f280df178aa564b3f95183008
James Douglas <james@douglastechnology.co.uk>
2019-03-23 18:46:36 +0000

    commit

--- src/popup.js
+++ src/popup.js
@@ -36,25 +36,21 @@ $('#subdomain').prop('href', browser.extension.getURL("src/manage.html?create=do
   browser.storage.local.get(function(item) {
     var styles = objectLength(item) - 1;
     for (var b = 1; b <= styles; b++) {
-      if (item["styling_"+b] && item["styling_"+b].disabled != "true" && item.disabled == "false") {
-        var blocks = objectLength(item["styling_"+b]) - 2;
-        block:
-        for (var c = 1; c <= blocks; c++) {
-          var urls = (objectLength(item["styling_"+b]["block_"+c]) - 1) / 2;
-          for (var d = 1; d <= urls; d++) { 
-            if (item["styling_"+b].block_1["url_"+d] != undefined && ((item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "url" && item["styling_"+b]["block_"+c]["url_"+d] == currentURL) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "starting" && currentURL.startsWith(item["styling_"+b]["block_"+c]["url_"+d])) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "domain" && item["styling_"+b]["block_"+c]["url_"+d] == getDomain(currentURL)) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "everything"))) {
-              var styleTitle = "style_"+b+"_name";
-              $.extend(true, active_styles, { [styleTitle]: item["styling_"+b].name });
-              break block;
-            }
+      var blocks = objectLength(item["styling_"+b]) - 2;
+      block:
+      for (var c = 1; c <= blocks; c++) {
+        var urls = (objectLength(item["styling_"+b]["block_"+c]) - 1) / 2;
+        for (var d = 1; d <= urls; d++) { 
+          if (item["styling_"+b].block_1["url_"+d] != undefined && ((item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "url" && item["styling_"+b]["block_"+c]["url_"+d] == currentURL) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "starting" && currentURL.startsWith(item["styling_"+b]["block_"+c]["url_"+d])) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "domain" && item["styling_"+b]["block_"+c]["url_"+d] == getDomain(currentURL)) || (item["styling_"+b]["block_"+c]["url_"+d+"_type"] == "everything"))) {
+            var styleTitle = "style_"+b+"_name";
+            $.extend(true, active_styles, { [styleTitle]: item["styling_"+b].name });
+            break block;
           }
         }
       }
     }
     console.log(active_styles);
-    for (x = 1; x <= objectLength(active_styles); x++) {
-      $('#active-syles').append('<input type="checkbox"><div>'+active_styles["style_"+x+"_name"]+'</div>');
-    }
+    for (x = 1; x <= objectLength(active_styles); x++) { $('#active-styles').append('<input type="checkbox"><div>'+active_styles["style_"+x+"_name"]+'</div>'); }
   });
 $(function() {
   $('img, label').click(function() { $('#disable').click(); });

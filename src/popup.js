425e21ddb3e5ddadff1c0eb48e98e14d8f3e31fe
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:10:18 +0000

    commit

--- src/popup.js
+++ src/popup.js
@@ -8,19 +8,17 @@ $(function() {
   function onChange() { console.log("changed"); }
   function onError(error) { console.log(`Error: ${error}`); }
   $.getJSON('../manifest.json', function(data) { $('#version').text(data.version); });
-  console.log(browser.storage.local.get("disabled").then(onChange, onError));
-  var disabled = { value: "false" };
   $('#disable').click(function() {
     if ($(this).is(':checked')) {
       $('img').prop("src", "../images/StylingDisabled.png");
-      disabled = { value: "true" };
       browser.browserAction.setIcon({path: "../images/StylingDisabled.png"});
-      browser.storage.local.set({disabled}).then(onChange, onError);
+      browser.storage.local.set({ disabled: { value: "true" } }).then(onChange, onError);
+      console.log(browser.storage.local.get("disabled").then(onChange, onError));
     } else {
       $('img').prop("src", "../images/Styling.png");
-      disabled = { value: "false" };
       browser.browserAction.setIcon({path: "../images/Styling.png"});
-      browser.storage.local.set({disabled}).then(onChange, onError);
+      browser.storage.local.set({ disabled: { value: "true" } }).then(onChange, onError);
+      console.log(browser.storage.local.get("disabled").then(onChange, onError));
     }
   });
   $('img, label').click(function() { $('#disable').click(); });

2bbce253b9b81322fecbf3b18f97cb9ca3627dbc
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:32:31 +0000

    commit

--- src/inject.js
+++ src/inject.js
@@ -12,9 +12,9 @@ function updateStyles() {
   let localStorage = browser.storage.local.get(function(item) {
     if (!item.disabled) { browser.storage.local.set({ disabled: "false" }).then(onChange, onError); }
     if (!item.styling_1) { browser.storage.local.set({ styling_1: { block_1: { code: "", url_1: "", url_1_type: "url" } } }).then(onChange, onError); }
+    var elements = document.getElementsByClassName('styling');
+    for (var i = 0; i < elements.length; i++) { document.getElementsByTagName("html")[0].removeChild(elements[i]); }
     if (item.styling_1 && item.styling_1.disabled != "true") {
-      var elements = document.getElementsByClassName('styling');
-      for (var i = 0; i < elements.length; i++) { document.getElementsByTagName("html")[0].removeChild(elements[i]); }
       if (item.disabled == "false") {
         var urls = (objectLength(item.styling_1.block_1) - 1) / 2;
         for (var a = 1; a <= urls; a++) { 

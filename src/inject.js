8b7ca5626bda75ca95a64b8409abaad875606ef9
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:00:29 +0000

    commit

--- src/inject.js
+++
@@ -1,7 +0,0 @@
-styleElement = document.createElement("style");
-styleElement.setAttribute("id", "styling-id");
-styleElement.setAttribute("class", "styling");
-styleElement.setAttribute("type", "text/css");
-styleElement.appendChild(document.createTextNode("body { background-color: yellow; }"));
-document.getElementsByTagName('html')[0].insertAfter(styleElement);
-ROOT.document.getElementsByTagName('html')[0].insertAfter(styleElement);

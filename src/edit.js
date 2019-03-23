a5a14b76067d298635d55a623c58d4e5d2f7b6ba
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:16:00 +0000

    commit

--- src/edit.js
+++ src/edit.js
@@ -14,11 +14,6 @@ function numOfLines(textArea, lineHeight) {
 }
 function updateTextarea() {
   var lines = numOfLines($('textarea.code'), document.querySelector("textarea").style.lineHeight.slice(0, 2));
-  if (lines < 15) { lines = 15; }
-  if (document.querySelector("textarea").value.split(/\r*\n/).length < 15) {
-    var toBeAdded = 15 - document.querySelector("textarea").value.split(/\r*\n/).length;
-    for (a = 1; a <= toBeAdded; a++) { var currentText = $('textarea').val(); $('textarea').val(currentText + '\n'); }
-  }
   if (lines != $('.side *').length) {
     $('.side').empty();
     for (a = 1; a <= lines; a++) { $('.side').append('<span class="line">'+a+'</span>'); }
@@ -39,12 +34,10 @@ $(function() {
     browser.storage.local.set({ styling_1: { block_1: { url_1: $('input.url').val(), url_1_type: $('select').val(), code_1: $('textarea.code').val() } } }).then(onChange, onError); 
     browser.tabs.query({ currentWindow: true }).then(sendMessageToTabs).catch(onError);
   });
-  $('textarea').on('scroll', function () {
-    $('.side').scrollTop($(this).scrollTop());
-  });
+  $('textarea').on('scroll', function () { $('.side').scrollTop($(this).scrollTop()); });
   $('#back').click(function() { window.location.replace("manage.html"); });
   $('select').change(function() { if ($(this).val() == "everything") { $('input.url').hide(); } else { $('input.url').show(); } });
-  $('textarea').bind('input propertychange', function() { updateTextarea(); });
+  $('textarea').bind('input propertychange', function() { updateTextarea(); }).resize(function() { $('.side').css('height', $('textarea').css('height')); });
   $(document).on('click', '.add', function() { $(this).parent().clone().children('select').val('url').parent().find('input').val('').end().insertAfter($(this).parent()); });
   $(document).on('click', '.remove', function() { if ($(this).parent().parent().children('.controls').length > 1) { $(this).parent().remove(); } });
 });

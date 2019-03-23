8d36562e5c4fd3479c8aa8628a8699fbe224ceb6
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:53:08 +0000

    commit

--- src/popup.js
+++ src/popup.js
@@ -5,6 +5,7 @@ function onGot(item) {
 }
 function onChange() { }
 function onError(error) { console.log(`Error: ${error}`); }
+function getDomain(url) { url = url.replace(/(https?:\/\/)?(www.)?/i, ''); if (url.indexOf('/') !== -1) { return url.split('/')[0]; } return url; }
 function sendDisableToTabs(tabs) { for (let tab of tabs) { browser.tabs.sendMessage(tab.id, {message: "all styles disabled"}).then(response => {  }).catch(onError); } }
 function sendEnableToTabs(tabs) { for (let tab of tabs) { browser.tabs.sendMessage(tab.id, {message: "all styles enabled"}).then(response => {  }).catch(onError); } }
 $.getJSON('../manifest.json', function(data) { $('#version').text(data.version); });
@@ -21,8 +22,11 @@ $('#disable').change(function() {
     browser.tabs.query({ currentWindow: true }).then(sendEnableToTabs).catch(onError);
   }
 });
+$('#url').prop('title', window.location.href);
+$('#domain').prop('title', getDomain(window.location.href));
+$('#subdomain').prop('title', getDomain(window.location.href));
 $(function() {
   $('img, label').click(function() { $('#disable').click(); });
   $('#manage').click(function() { browser.tabs.create({ url:"manage.html" }).then(onChange, onError); });
-  $('#details').click(function() { browser.tabs.create({ url:"manage.html" }).then(onChange, onError); });
+  $('#details').click(function() { chrome.runtime.openOptionsPage(); window.close(); });
 });

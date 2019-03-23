7984ff63562918e4d53651a850fcebb961188ca5
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:13:37 +0000

    commit

--- src/inject.js
+++ src/inject.js
@@ -7,10 +7,10 @@ function updateStyles() {
       browser.storage.local.set({ disabled: "false" }).then(onChange, onError);
     }
     if (item.styling_1) {
-      function apply() {
-        var elements = document.getElementsByClassName('styling');
-        for (var i = 0; i < elements.length; i++) { document.getElementsByTagName("html")[0].removeChild(elements[i]); }
-        if (item.disabled == "false") {
+      var elements = document.getElementsByClassName('styling');
+      for (var i = 0; i < elements.length; i++) { document.getElementsByTagName("html")[0].removeChild(elements[i]); }
+      if (item.disabled == "false") {
+        if (item.styling_1.block_1.url_1 != undefined && ((item.styling_1.block_1.url_1_type == "url" && item.styling_1.block_1.url_1 == window.location.href) || (item.styling_1.block_1.url_1_type == "starting" && window.location.href.startsWith(item.styling_1.block_1.url_1)) || (item.styling_1.block_1.url_1_type == "domain" && item.styling_1.block_1.url_1 == location.hostname) || (item.styling_1.block_1.url_1_type == "everything"))) {
           var styleElement = document.createElement("style");
           styleElement.setAttribute("id", "styling-1");
           styleElement.setAttribute("class", "styling");
@@ -18,9 +18,6 @@ function updateStyles() {
           styleElement.appendChild(document.createTextNode(item.styling_1.block_1.code_1));
           document.getElementsByTagName('html')[0].appendChild(styleElement);
         }
-      }
-      if (item.styling_1.block_1.url_1 != undefined && ((item.styling_1.block_1.url_1_type == "url" && item.styling_1.block_1.url_1 == window.location.href) || (item.styling_1.block_1.url_1_type == "starting" && window.location.href.startsWith(item.styling_1.block_1.url_1)) || (item.styling_1.block_1.url_1_type == "domain" && item.styling_1.block_1.url_1 == location.hostname) || (item.styling_1.block_1.url_1_type == "everything"))) {
-        apply();
       }
     }
   });

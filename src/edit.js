058923478c40aa279aabf0042eedd7665665b1bd
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:14:07 +0000

    commit

--- src/edit.js
+++ src/edit.js
@@ -12,19 +12,19 @@ function numOfLines(textArea, lineHeight) {
   return Math.ceil(scrollHeight / lineHeight);
 }
 $(function() {
-  numOfLines(document.querySelector("textarea"), toString($('textarea').css('line-height')).slice(0, -2));
+  var lines = numOfLines(document.querySelector("textarea"), toString($('textarea').css('line-height')).slice(0, -2));
   browser.storage.local.get().then(function(item) { 
     if (item.styling_1) {
-      if (item.styling_1.block_1.url_1) { $('input').val(item.styling_1.block_1.url_1); }
+      if (item.styling_1.block_1.url_1) { $('input.url').val(item.styling_1.block_1.url_1); }
       if (item.styling_1.block_1.url_1_type) { $('option[value='+item.styling_1.block_1.url_1_type+']').prop('selected', 'true'); }
-      if (item.styling_1.block_1.url_1_type == 'everything') { $('input').hide(); }
-      if (item.styling_1.block_1.code_1) { $('textarea').text(item.styling_1.block_1.code_1); }
+      if (item.styling_1.block_1.url_1_type == 'everything') { $('input.url').hide(); }
+      if (item.styling_1.block_1.code_1) { $('textarea.code').text(item.styling_1.block_1.code_1); }
     }
   });
   $('#save').click(function() {
-    browser.storage.local.set({ styling_1: { block_1: { url_1: $('.url').val(), url_1_type: $('select').val(), code_1: $('.code').val() } } }).then(onChange, onError); 
+    browser.storage.local.set({ styling_1: { block_1: { url_1: $('input.url').val(), url_1_type: $('select').val(), code_1: $('input.code').val() } } }).then(onChange, onError); 
     browser.tabs.query({ currentWindow: true }).then(sendMessageToTabs).catch(onError);
   });
   $('#back').click(function() { window.location.replace("manage.html"); });
-  $('select').change(function() { if ($(this).val() == "everything") { $('input').hide(); } else { $('input').show(); } });
+  $('select').change(function() { if ($(this).val() == "everything") { $('input.url').hide(); } else { $('input.url').show(); } });
 });

f901f613c59e11f8afc00d6ca111d1b1f2abe2f7
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:10:55 +0000

    commit

--- src/manage.js
+++ src/manage.js
@@ -1,7 +1,10 @@
 $(function() {
   function onChange() { /*console.log("changed");*/ }
   function onError(error) { console.log(`Error: ${error}`); }
-  $('textarea').text(browser.storage.local.get().stylish_1.code);
+  browser.storage.local.get().then(function(item) {
+  	code = item.stylish_1.code;
+  });
+  $('textarea').text(code);
   $('#update').click(function() {
     var stylingCode = { value: document.getElementById("code").value };
     browser.storage.local.set({ stylish_1: { code: stylingCode } }).then(onChange, onError); 
--- src/popup.js
+++ src/popup.js
@@ -1,24 +1,25 @@
-$(function() {
-  browser.storage.local.get().then(onGot, onError);
-  function onGot(item) { 
-    if (item.disabled.value == "true") { $('img').prop("src", "../images/StylingDisabled.png"); $('input[type=checkbox]').prop("checked", true);
-    } else { $('img').prop("src", "../images/Styling.png");  $('input[type=checkbox]').prop("checked", false); }
+browser.storage.local.get().then(onGot, onError);
+function onGot(item) { 
+  if (item.disabled.value == "true") { $('img').prop("src", "../images/StylingDisabled.png"); $('input[type=checkbox]').prop("checked", true);
+  } else { $('img').prop("src", "../images/Styling.png");  $('input[type=checkbox]').prop("checked", false); }
+}
+function onChange() { /*console.log("changed");*/ }
+function onError(error) { console.log(`Error: ${error}`); }
+$.getJSON('../manifest.json', function(data) { $('#version').text(data.version); });
+$('#disable').change(function() {
+  if ($(this).is(':checked')) {
+    $('img').prop("src", "../images/StylingDisabled.png");
+    browser.browserAction.setIcon({path: "../images/StylingDisabled.png"});
+    browser.storage.local.set({ disabled: { value: "true" } }).then(onChange, onError);
+    updateStyles();
+  } else {
+    $('img').prop("src", "../images/Styling.png");
+    browser.browserAction.setIcon({path: "../images/Styling.png"});
+    browser.storage.local.set({ disabled: { value: "false" } }).then(onChange, onError);
+    updateStyles();
   }
-  function onChange() { /*console.log("changed");*/ }
-  function onError(error) { console.log(`Error: ${error}`); }
-  $.getJSON('../manifest.json', function(data) { $('#version').text(data.version); });
-  $('#disable').change(function() {
-    if ($(this).is(':checked')) {
-      $('img').prop("src", "../images/StylingDisabled.png");
-      browser.browserAction.setIcon({path: "../images/StylingDisabled.png"});
-      browser.storage.local.set({ disabled: { value: "true" } }).then(onChange, onError);
-
-    } else {
-      $('img').prop("src", "../images/Styling.png");
-      browser.browserAction.setIcon({path: "../images/Styling.png"});
-      browser.storage.local.set({ disabled: { value: "false" } }).then(onChange, onError);
-    }
-  });
+});
+$(function() {
   $('img, label').click(function() { $('#disable').click(); });
   $('#manage').click(function() { browser.tabs.create({ url:"manage.html" }).then(onChange, onError); });
   $('#details').click(function() { browser.tabs.create({ url:"manage.html" }).then(onChange, onError); });

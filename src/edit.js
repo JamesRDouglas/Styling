314169b3e6c025d835cb8ebe378779a79fd6d601
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:36:28 +0000

    commit

--- src/edit.js
+++ src/edit.js
@@ -2,7 +2,7 @@ function onChange(item) {}
 function onError(error) { console.log(`Error: ${error}`); }
 function sendMessageToTabs(tabs) { for (let tab of tabs) { browser.tabs.sendMessage(tab.id, { message: "styles updated" }).then(response => {}).catch(onError); } }
 function objectLength(object) { var length = 0; for(var key in object) { if( object.hasOwnProperty(key) ) { ++length; } } return length; };
-function updateTextarea() { for (var a = 1; a <= $('.block').length; a++) { $('.block:nth-of-type('+a+')').prop('id', 'block_'+a).children('span:nth-of-type(2)').text(a); } }
+function updateBlocks() { for (var a = 1; a <= $('.block').length; a++) { $('.block:nth-of-type('+a+')').prop('id', 'block_'+a).children('span:nth-of-type(2)').text(a); $('.block:nth-of-type('+a+')').find('div.code').prop('id', 'code_'+a); } }
 $(function() {
   browser.storage.local.get().then(function(item) { 
     if (item.styling_1) {
@@ -16,12 +16,12 @@ $(function() {
           if (item.styling_1["block_"+e]["url_"+a+"_type"]) { $('body .block:nth-of-type('+e+') section:nth-of-type('+a+') select').val(item.styling_1["block_"+e]["url_"+a+"_type"]); }
           if (item.styling_1["block_"+e]["url_"+a+"_type"] == 'everything') { $('body .block:nth-of-type('+e+') section:nth-of-type('+a+') input.url').hide(); }
         }
-        if (item.styling_1["block_"+e].code) { $('body .block:nth-of-type('+e+') textarea.code').val(item.styling_1["block_"+e].code); updateTextarea(); }
+        if (item.styling_1["block_"+e].code) { $('body .block:nth-of-type('+e+') textarea.code').val(item.styling_1["block_"+e].code); updateBlocks(); }
         if (item.styling_1.disabled === "true") { $('#enabled').prop('checked', false); } else { $('#enabled').prop('checked', true); }
         if (item.disabled === "true") { $('#enabled').prop('disabled', true); } else { $('#enabled').prop('disabled', false); }
       }
     }
-    ace.edit($('textarea.code'), { mode: "ace/mode/javascript", selectionStyle: "text" });
+    for (var a = 1; a <= $('.block').length; a++) { ace.edit('code_'+a, { mode: "ace/mode/css", selectionStyle: "text" }); }
     $('#enabled').click(function() { 
       var code = item; 
       if ($('#enabled').is(':checked')) { 
@@ -35,7 +35,7 @@ $(function() {
       } 
     });
   });
-  updateTextarea();
+  updateBlocks();
   $('#save').click(function() {
     for (var c = 1; c <= $('div.block').length; c++) {
       var blockName = "block_"+c;
@@ -57,12 +57,12 @@ $(function() {
   });
   $('#back').click(function() { window.location.replace("manage.html"); });
   $(document).on('change', 'select', function() { if ($(this).val() == "everything") { $(this).next('input.url').hide(); $(this).parent().addClass('current').parent().children('div.controls:not(.current)').remove(); } else { $(this).next('input.url').show(); } });
-  $(document).on('input', 'textarea', function() { updateTextarea(); });
+  $(document).on('input', 'textarea', function() { updateBlocks(); });
   $(document).on('click', '.add_controls', function() { $(this).parent().clone().find('select').val($(this).parent().find('select').val()).end().find('input.url').val('').end().insertAfter($(this).parent()); });
   $(document).on('click', '.remove_controls', function() { if ($(this).parent().parent().children('.controls').length > 1) { $(this).parent().remove(); } });
-  $(document).on('click', '.add_block', function() { $(this).parent().clone().find('textarea.code').val('').end().find('input').val('').end().find('section:not(:first-of-type)').remove().end().prop('id', '').insertAfter($(this).parent()); updateTextarea(); });
-  $(document).on('click', '.remove_block', function() { $(this).parent().remove(); updateTextarea(); });
-  $(document).on('click', '.clone_block', function() { $(this).parent().clone().find('select').val($(this).parent().find('select').val()).end().insertAfter($(this).parent()); updateTextarea(); });
+  $(document).on('click', '.add_block', function() { $(this).parent().clone().find('textarea.code').val('').end().find('input').val('').end().find('section:not(:first-of-type)').remove().end().prop('id', '').insertAfter($(this).parent()); updateBlocks(); });
+  $(document).on('click', '.remove_block', function() { $(this).parent().remove(); updateBlocks(); });
+  $(document).on('click', '.clone_block', function() { $(this).parent().clone().find('select').val($(this).parent().find('select').val()).end().insertAfter($(this).parent()); updateBlocks(); });
 });
 browser.runtime.onMessage.addListener(function(message) { if (message.message === "all styles disabled") { $('#enabled').prop('disabled', true); } else if (message.message === "all styles enabled") { $('#enabled').prop('disabled', false); } });
 

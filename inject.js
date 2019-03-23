d74ce2b8b6faaf167bce6870df1a5f4cd983953d
James Douglas <james@douglastechnology.co.uk>
2019-03-23 16:17:57 +0000

    commit

--- inject.js
+++ inject.js
@@ -3,5 +3,5 @@ styleElement.setAttribute("id", "styling-id");
 styleElement.setAttribute("class", "styling");
 styleElement.setAttribute("type", "text/css");
 styleElement.appendChild(document.createTextNode("body { background-color: yellow; }"));
-document.getElementsByTagName('html')[0].appendChild(styleElement);
-ROOT.document.getElementsByTagName('html')[0].appendChild(styleElement);
+//document.getElementsByTagName('html')[0].insertAfter(styleElement);
+ROOT.document.getElementsByTagName('html')[0].insertAfter(styleElement);

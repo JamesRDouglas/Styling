5c2ae43bcdc9acbe79202be1776a4587a8b633a8
James Douglas <james@douglastechnology.co.uk>
2019-03-23 18:54:09 +0000

    commit

--- src/manage.js
+++ src/manage.js
@@ -3,12 +3,12 @@ function onError(error) { console.log(`${error}`); }
 function objectLength(object) { var length = 0; for(var key in object) { if( object.hasOwnProperty(key) ) { ++length; } } return length; };
 function sendMessageToTabs(tabs) { for (let tab of tabs) { browser.tabs.sendMessage(tab.id, { message: "update scripts" }).then(response => {}).catch(onError); } }
 function checkStyleExists(b, item, styles_arr) { if (item["styling_"+b]) { styles_arr.push(b); $('#content').append('<div class="style" id="style_'+b+'" data-id="'+b+'"><input type="checkbox"><span class="name" title="'+item["styling_"+b].name+'">'+item["styling_"+b].name+'</span><button class="edit" data-id="'+b+'">Edit</button><button class="delete" data-id="'+b+'">Delete</button><div class="url_list"></div></div>'); b++; } else { b++; checkStyleExists(b, item, styles_arr); } return b; }
-function loadStyles() { browser.storage.local.get().then(function(item) { var styles = objectLength(item) - 1, b = 1; for (a = 1; a <= styles; a = a) { if (a > styles) { break; } if (item["styling_"+b] !== undefined) { a++; } b = checkStyleExists(b, item, styles_arr); } }); }
+function loadStyles(styles_arr) { browser.storage.local.get().then(function(item) { var styles = objectLength(item) - 1, b = 1; for (a = 1; a <= styles; a = a) { if (a > styles) { break; } if (item["styling_"+b] !== undefined) { a++; } b = checkStyleExists(b, item, styles_arr); } }); }
 $(function() {
   var delete_id = new URLSearchParams(window.location.search).get('delete');
   if (delete_id) { browser.storage.local.get().then(function(item) { var currentStyle = 'styling_'+delete_id; browser.storage.local.remove(currentStyle).then(onChange, onError); window.location.href = "manage.html"; }); }
   var styles_arr = [];
-  loadStyles();
+  loadStyles(styles_arr);
   $('#write-new').click(function() { for (a = 1; styles_arr.indexOf(a) === -1; a++) { window.location.href = "edit.html?style="+a; } });
   $(document).on('click', '.style', function() { window.location.href = "edit.html?style="+$(this).data("id"); });
   $(document).on('click', '.style > input, .url_list', function(e) { e.stopPropagation(); });

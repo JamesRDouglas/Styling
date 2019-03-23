49190956e8a44dc395924d81c0a7074731e79c73
James Douglas <james@douglastechnology.co.uk>
2019-03-23 16:17:49 +0000

    commit

--- options.js
+++ options.js
@@ -1,17 +1,18 @@
-function setItem() { console.log("OK"); }
-function onError(error) { console.log(error) }
+function setItem() { 
+  document.getElementById("url").value = `${item.styling_url.value}`;
+  document.getElementById("code").value = `${item.styling_code.value}`;
+}
+function onError(error) { 
+  console.log(error) 
+  var styling_url = "*";
+  var styling_code = "background-color: yellow;";
+  browser.storage.local.set({styling_url, styling_code}).then(setItem(), onError());
+}
 function update() { 
+  alert('hi');
   var styling_url = { value: document.getElementById("url").value }
   var styling_code = { value: document.getElementById("code").value }
   browser.storage.local.set({styling_url, styling_code}).then(setItem(), onError()); 
 }
-if (!browser.storage.local.get("styling_url")) {
-  var styling_url = "*";
-  var styling_code = "background-color: yellow;";
-  browser.storage.local.set({styling_url, styling_code}).then(setItem(), onError());
-} else {
-  browser.storage.local.get("styling_url").then(setItem(), onError());
-  browser.storage.local.get("styling_code").then(setItem(), onError());
-  document.getElementById("url").value = `${item.styling_url.value}`;
-  document.getElementById("code").value = `${item.styling_code.value}`;
-}
+browser.storage.local.get("styling_url").then(setItem(), onError());
+browser.storage.local.get("styling_code").then(setItem(), onError());

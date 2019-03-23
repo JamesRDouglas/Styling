5ade3d6ae9c63f88729c3b026159748192a5b26f
James Douglas <james@douglastechnology.co.uk>
2019-03-23 17:10:13 +0000

    commit

--- src/popup.js
+++ src/popup.js
@@ -9,16 +9,16 @@ $(function() {
   function onError(error) { console.log(`Error: ${error}`); }
   $.getJSON('../manifest.json', function(data) { $('#version').text(data.version); });
   console.log(browser.storage.local.get("disabled").then(onChange, onError));
-  var disabled = { state: "false" };
+  var disabled = { value: "false" };
   $('#disable').click(function() {
     if ($(this).is(':checked')) {
       $('img').prop("src", "../images/StylingDisabled.png");
-      disabled = { state: "true" };
+      disabled = { value: "true" };
       browser.browserAction.setIcon({path: "../images/StylingDisabled.png"});
       browser.storage.local.set({disabled}).then(onChange, onError);
     } else {
       $('img').prop("src", "../images/Styling.png");
-      disabled = { state: "false" };
+      disabled = { value: "false" };
       browser.browserAction.setIcon({path: "../images/Styling.png"});
       browser.storage.local.set({disabled}).then(onChange, onError);
     }

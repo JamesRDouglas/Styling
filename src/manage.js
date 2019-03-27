6fd14aa3d8b98e4699dee02489191bce1840d4c9
James Douglas <james@douglastechnology.co.uk>
2019-03-27 18:01:58 +0000

    commit

--- src/manage.js
+++ src/manage.js
@@ -10,7 +10,7 @@ function saveOptions() {
 $(function() {
   var styles_arr = [];
   browser.storage.local.get().then(function(item) {
-    if (!item.default || !item.default.name || !item.default.disabled || !item.default.blocks) { browser.storage.local.set({ default: { name: "new style", id: "1", disabled: false, blocks: [ { code: "", urls: [ { address: "", type: "" } ] } ] } }).then(onDone, onError); default_style = item.default; }
+    if (!item.default || !item.default.name || !item.default.disabled || !item.default.blocks) { item.default = { name: "new style", id: "1", disabled: false, blocks: [ { code: "", urls: [ { address: "", type: "" } ] } ] }; }
     if (!item.default.options) { item.default.options = { tab_size: "2", font_size: "11", line_count: "15", autocomplete: true, error_marker: true, soft_tabs: true, guide_indent: false, show_invisible: false, keybinding: "default" }; browser.storage.local.set(item).then(onDone, onError); default_style = item.default; }
     if (!item.styles[0]) { item.styles[0] = default_style; browser.storage.local.set(item).then(onDone, onError); }
     $('#line-count').val(item.default.options.line_count);

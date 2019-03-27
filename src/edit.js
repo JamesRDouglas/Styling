da3da21ff02c806e9f162043a62178b10585885f
James Douglas <james@douglastechnology.co.uk>
2019-03-27 18:05:44 +0000

    commit

--- src/edit.js
+++ src/edit.js
@@ -68,14 +68,7 @@ $(function() {
   $(document).on('click', '.remove_block', function() { $(this).parent().remove(); updateBlocks(); });
   $(document).on('click', '.raise_block', function() { $(this).parent().insertBefore($(this).parent().prev()); updateBlocks(); });
   $(document).on('click', '.lower_block', function() { $(this).parent().insertAfter($(this).parent().next()); updateBlocks(); });
-  $(document).on('click', '.clone_block', function() {
-    $(this).parent().find('.add_block').click();
-    $(this).parent().next().find('section').remove();
-    $(this).parent().clone().find('section').insertAfter($(this).parent().next().find('.code'));
-    var clonedText = ace.edit($(this).parent().find('.code')[0].id).getValue();
-    ace.edit($(this).parent().next().find('.code')[0].id).setValue(clonedText, -1);
-    updateBlocks();
-  });
+  $(document).on('click', '.clone_block', function() { $(this).parent().find('.add_block').click(); $(this).parent().next().find('section').remove(); $(this).parent().clone().find('section').insertAfter($(this).parent().next().find('.code')); ace.edit($(this).parent().next().find('.code')[0].id).setValue(ace.edit($(this).parent().find('.code')[0].id).getValue(), -1); updateBlocks(); });
   $(document).on('click', '.beautify_block', function() { ace.edit($(this).parent().find('div.code')[0]).setValue(css_beautify(ace.edit($(this).parent().find('div.code')[0]).getValue(), { 'indent_size': 2, 'selector_separator_newline': false, 'space_around_selector_separator': true })); });
   $(document).on('click', '.add_target', function() { $(this).parent().clone().find('select').val($(this).parent().find('select').val()).end().find('input.url').val('').end().insertAfter($(this).parent()); });
   $(document).on('click', '.remove_target', function() { if ($(this).parent().parent().children('.target').length > 1) { $(this).parent().remove(); } });

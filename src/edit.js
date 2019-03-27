9a0809c79be84aa7c6979ee338f47fde096918b7
James Douglas <james@douglastechnology.co.uk>
2019-03-27 18:03:15 +0000

    commit

--- src/edit.js
+++ src/edit.js
@@ -52,10 +52,10 @@ $(function() {
         for (c = 0; c < $('div.block').length; c++) {
           var urls = $('div.block:nth-of-type('+(c+1)+')').children('section').length, code = ace.edit("code_"+(c+1)).getValue().replace(/^|\s+$/g, '');
           item.styles[style_id].blocks.push({ code: code, urls: [] });
-          /*for (b = 0; b < urls; b++) { 
+          for (b = 0; b < urls; b++) { 
             var address = $('div.block:nth-of-type('+(c+1)+')').find('section:nth-of-type('+(b+1)+')').children('input.url').val(), type = $('div.block:nth-of-type('+(c+1)+')').find('section:nth-of-type('+(b+1)+')').children('select').val();
-            item.styles[style_id].blocks[b].urls.push({ address: address, type: type });
-          }*/
+            item.styles[style_id].blocks[c].urls.push({ address: address, type: type });
+          }
         }
         browser.storage.local.set(item).then(onDone, onError);
       });

5a504d87c0cd148bc3ac805d00f9865af769f15a
James Douglas <james@douglastechnology.co.uk>
2019-03-27 16:04:52 +0000

    commit

--- src/background.js
+++
@@ -1,7 +0,0 @@
-browser.storage.local.get(function(item) {
-  if (item.disabled == "true") {
-  	browser.browserAction.setIcon({path: "../images/StylingDisabled.png"});
-  } else {
-    browser.browserAction.setIcon({path: "../images/Styling.png"});
-  }
-});

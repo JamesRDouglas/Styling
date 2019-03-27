51f7d00c8a004a3e19dd7741fb3c144bb77b6ec7
James Douglas <james@douglastechnology.co.uk>
2019-03-27 18:06:09 +0000

    commit

--- src/manage.js
+++ src/manage.js
@@ -1,11 +1,11 @@
 function onDone(item) { }
 function onError(error) { console.log(error); }
 function sendMessageToTabs(tabs, message) { for (let tab of tabs) { if (message === "disable") { browser.tabs.sendMessage(tab.id, {message: "styles disabled"}).then(response => {  }).catch(onError); } else if (message === "enable") { browser.tabs.sendMessage(tab.id, {message: "styles enabled"}).then(response => {  }).catch(onError); } else if (message === "update") { browser.tabs.sendMessage(tab.id, {message: "styles updated"}).then(response => {  }).catch(onError); } } }
-function saveOptions() { 
-  browser.storage.local.get().then(function(item) { 
-    item.default.options = { tab_size: $('#tab-size').val(), font_size: $('#font-size').val(), line_count: $('#line-count').val(), autocomplete: $('#autocomplete').prop('checked'), error_marker: $('#error-marker').prop('checked'), soft_tabs: $('#soft-tabs').prop('checked'), guide_indent: $('#guide-indent').prop('checked'), show_invisible: $('#show-invisible').prop('checked'), keybinding: $('#keybinding').val() }; 
-    browser.storage.local.set(item).then(onDone, onError); 
-  }); 
+function saveOptions() {
+  browser.storage.local.get().then(function(item) {
+    item.default.options = { tab_size: $('#tab-size').val(), font_size: $('#font-size').val(), line_count: $('#line-count').val(), autocomplete: $('#autocomplete').prop('checked'), error_marker: $('#error-marker').prop('checked'), soft_tabs: $('#soft-tabs').prop('checked'), guide_indent: $('#guide-indent').prop('checked'), show_invisible: $('#show-invisible').prop('checked'), keybinding: $('#keybinding').val() };
+    browser.storage.local.set(item).then(onDone, onError);
+  });
 }
 $(function() {
   var styles_arr = [];
@@ -23,14 +23,26 @@ $(function() {
     if (item.default.options.guide_indent === true) { $('#guide-indent').prop("checked", true); }
     if (item.default.options.show_invisible === true) { $('#show-invisible').prop("checked", true); }
     $('#keybinding').val(item.default.options.keybinding);
-    for (a = 0; a < item.styles.length; a = a) { if (item.styles[a]) { styles_arr.push(item.styles[a].id); $('#content').append('<div class="style" id="style_'+styles_arr[a]+'" data-id="'+styles_arr[a]+'"><input type="checkbox"><span class="name" title="'+item.styles[a].name+'">'+item.styles[a].name+'</span><button class="edit" data-id="'+styles_arr[a]+'">Edit</button><button class="delete" data-id="'+a+'">Delete</button><div class="url_list"></div></div>'); a++; } }
+    for (a = 0; a < item.styles.length; a = a) { if (item.styles[a]) { styles_arr.push(item.styles[a].id); $('#content').append('<div class="style" id="style_'+styles_arr[a]+'" data-id="'+styles_arr[a]+'"><input type="checkbox" class="check"><span class="name" title="'+item.styles[a].name+'">'+item.styles[a].name+'</span><button class="edit" data-id="'+styles_arr[a]+'">Edit</button><button class="delete" data-id="'+a+'">Delete</button><div class="url_list"></div></div>'); a++; } }
   });
   $('#write-new').click(function() { window.location.href = "edit.html?style=new"; });
-  $(document).on('click', '.style', function() { window.location.href = "edit.html?style="+$(this).data("id"); });  
+  $(document).on('click', '.style', function() { window.location.href = "edit.html?style="+$(this).data("id"); });
   $(document).on('click', '.style > input, .url_list', function(e) { e.stopPropagation(); });
+  $(document).on('click', '.check', function() {
+    var style_id = $(this).nextAll().eq(2).data('id');
+      browser.storage.local.get(function(item) {
+        if ($(this).is(':checked')) {
+          item.styles[style_id].disabled = false;
+          browser.storage.local.set(item).then(onDone, onError);
+          browser.tabs.query({ currentWindow: true }).then(function(tabs) { sendMessageToTabs(tabs,"update"); }).catch(onError);
+        } else {
+          item.styles[style_id].disabled = true;
+          browser.storage.local.set(item).then(onDone, onError);
+          browser.tabs.query({ currentWindow: true }).then(function(tabs) { sendMessageToTabs(tabs,"update"); }).catch(onError);
+        }
+      });
+  });
   $(document).on('click', '.delete', function(e) { e.stopPropagation(); if (confirm('Are you sure you want to delete "'+$(this).parent().find('.name').prop("title")+'"?')) { $(this).parent().remove(); var style_id = $(this).data("id"); browser.storage.local.get(function(item) { item.styles.splice(style_id,1); browser.storage.local.set(item).then(onDone, onError); }); } });
   $(document).on('change', '.options', function() { saveOptions(); });
 });
 browser.runtime.onMessage.addListener(function(message) { if (message.message === "styles disabled") { $('#enabled').prop('disabled', true); } else if (message.message === "styles enabled") { $('#enabled').prop('disabled', false); } });
-
-
